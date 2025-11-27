6347281d1ac809628c67b596ab750de0103d78a9
Mafer <maria.roman.24s@utzmg.edu.mx>
2025-11-26 20:07:28 -0600

    first commit

---
+++ src/config/conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "planu";
+$user = "postgres";
+$password = "3312";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
